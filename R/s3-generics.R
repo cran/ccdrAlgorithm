32593bd3ec41135fe23103998bcca378f115eff0
Bryon Aragam <sparsebn@gmail.com>
2016-08-10 14:16:05 +0000

    version 0.0.1

---
+++ R/s3-generics.R
@@ -0,0 +1,18 @@
+#
+#  s3-generics.R
+#  ccdrAlgorithm
+#
+#  Created by Bryon Aragam (local) on 1/22/16.
+#  Copyright (c) 2014-2016 Bryon Aragam. All rights reserved.
+#
+
+#
+# PACKAGE CCDRALGORITHM: Generics
+#
+#   CONTENTS:
+#
+
+# Generics for SparseBlockMatrixR ------------------------------------------------------------
+SparseBlockMatrixR <- function(x, ...) UseMethod("SparseBlockMatrixR", x)
+as.SparseBlockMatrixR <- function(x) UseMethod("as.SparseBlockMatrixR", x)
+to_B <- function(x) UseMethod("to_B", x)
